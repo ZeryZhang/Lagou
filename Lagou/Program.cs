9559e3aa0019cf130bbb811a9d19c778921e5ce6
383606061@qq.com <383606061@qq.com>
2015-10-30 17:41:10 +0800

    ..

--- Lagou/Program.cs
+++ Lagou/Program.cs
@@ -28,9 +28,6 @@ namespace Lagou
             //webproxy.Address = uri;
             //request.Proxy = webproxy;
 
-
-
-
             request.Accept = "text/plain, */*; q=0.01";
             request.Method = "GET";
             request.Headers.Add("Accept-Language", "zh-cn,zh;q=0.8,en-us;q=0.5,en;q=0.3");
@@ -45,6 +42,23 @@ namespace Lagou
             string html = reader.ReadToEnd();
 
             return html;
+
+        }
+
+        public string GetJobType(string html)
+        {
+            
+            HtmlAgilityPack.HtmlDocument document = new HtmlDocument();
+            document.LoadHtml(html);
+            HtmlNodeCollection jobCatgroy = document.DocumentNode.SelectNodes("dl[@class='reset']"); //取大分类
+
+            foreach (var item in jobCatgroy)
+            {
+                item.SelectNodes("");
+            }
+
+
+            return string.Empty;
 
         }
 
