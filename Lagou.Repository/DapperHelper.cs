9abe1116d45d1d72386f357a52f91de81320ddae
383606061@qq.com <383606061@qq.com>
2015-11-12 20:52:20 +0800

    修改数据库连接

--- Lagou.Repository/DapperHelper.cs
+++ Lagou.Repository/DapperHelper.cs
@@ -13,14 +13,14 @@ namespace Lagou.Repository
     public class DapperHelper
     {
 
-        private static DbConnection connection;
+        
         private  string connectionString = "Data Source=192.168.0.19;Initial Catalog=HKLG;User ID=sa;Pwd=123456";
         //private readonly string connectionString = "Data Source=Zery-Zhang;Initial Catalog=HKLG;User ID=sa;Pwd=123456";
         
 
         public  DbConnection GetConnection()
         {
-            return connection = new SqlConnection(connectionString);
+            return new SqlConnection(connectionString);
         }
 
     }
